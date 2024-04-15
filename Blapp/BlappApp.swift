a4384d13bc96e16eaf9a32ccb6a41293a4f1691e
Aron Tiselius <aron.tiselius@gmail.com>
2024-04-15 11:43:11 +0200

    Initial Commit

---
+++ Blapp/BlappApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlappApp.swift
+//  Blapp
+//
+//  Created by Aron Tiselius on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct BlappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blapp
+//
+//  Created by Aron Tiselius on 2024-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
