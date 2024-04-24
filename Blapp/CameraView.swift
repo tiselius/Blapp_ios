e8333a05c9657d2656b92d1c0631d9b021af3cc3
Peter <peter.bystrom93@gmail.com>
2024-04-24 16:23:04 +0200

    !!!!!!

--- Blapp/CameraView.swift
+++ Blapp/CameraView.swift
@@ -10,45 +10,35 @@ import SwiftUI
 struct CameraView: View {
     @StateObject var frameHandler = FrameHandler()
 //    @StateObject private var frameHandler : FrameHandler
-    var image: CGImage?
-    @State private var isLoading = true
-    private let label = Text("frame")
+    //    @StateObject private var frameHandler : FrameHandler
+        var image: CGImage?
+        @State private var isLoading = true
+        private let label = Text("frame")
     
     var body: some View {
-<<<<<<< HEAD
-        
-        ZStack{
-            FrameView(image: model.frame)
-            //.frame(height: UIScreen.main.bounds.height * 0.8)
-                .aspectRatio(contentMode: .fit) // Ensure the image fits within the frame
-                .ignoresSafeArea()
-            
-            ContourDetectionView(frameHandler:  frameHandler)
-                .aspectRatio(contentMode: .fit)
-                .ignoresSafeArea()
-=======
         if let image = frameHandler.frame {
-            Image(image, scale: 1.0, orientation: .up, label: label)
-                .resizable()
-                
-        } else {
-            GeometryReader { geometry in
-                LinearGradient(
-                    gradient: Gradient(colors: [
-                        Color(red: 1, green: 0.39, blue: 0.39),
-                        Color(red: 1, green: 0.55, blue: 0.63),
-                        Color(red: 1, green: 0.56, blue: 0.64),
-                        Color(red: 1, green: 0.56, blue: 0.65),
-                        Color(red: 1, green: 0.57, blue: 0.66),
-                        Color(red: 1, green: 0.71, blue: 0.87)
-                    ]),
-                    startPoint: .top,
-                    endPoint: .bottom
-                )
-                .edgesIgnoringSafeArea(.all)
+                    Image(image, scale: 1.0, orientation: .up, label: label)
+                        .resizable()
+
+                } else {
+                    GeometryReader { geometry in
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                Color(red: 1, green: 0.39, blue: 0.39),
+                                Color(red: 1, green: 0.55, blue: 0.63),
+                                Color(red: 1, green: 0.56, blue: 0.64),
+                                Color(red: 1, green: 0.56, blue: 0.65),
+                                Color(red: 1, green: 0.57, blue: 0.66),
+                                Color(red: 1, green: 0.71, blue: 0.87)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                        .edgesIgnoringSafeArea(.all)
+                    }
+                }
             }
->>>>>>> LiveCamera
-        }
-    }
-}
+
+
+            }
 
