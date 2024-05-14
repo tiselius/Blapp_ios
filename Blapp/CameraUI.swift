e22c3b4ac5cf8dfeb6f0cfdf50955da8f5806f15
Peter <peter.bystrom93@gmail.com>
2024-05-14 10:07:33 +0200

    CamerUI

--- Blapp/CameraUI.swift
+++ Blapp/CameraUI.swift
@@ -17,38 +17,46 @@ struct CameraUI: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        
-        ZStack{
-            if(!self.useReference){
-                CameraView(frameHandler: frameHandler)
-                    .frame(height: UIScreen.main.bounds.height)
-                    .ignoresSafeArea()
-            }
-            else{
-                CameraViewReference(frameHandler: frameHandler)
-                    .frame(height: UIScreen.main.bounds.height)
-                    .ignoresSafeArea()
-            }
-
-            Text(showVolumeText ? "\(currentVolume) dl" : "")
-                .padding(30)
-                .foregroundColor(Color.white)
-                .background(Color(red: 1.0, green: 0.71, blue: 0.87))
-                .cornerRadius(10)
-                .shadow(radius: 2)
-                .offset(x: showVolumeText ? 0 : 1000)
-            
-            //Buttons
+        VStack(alignment: .center){
+            ZStack{
+                if(!self.useReference){
+                    CameraView(frameHandler: frameHandler)
+                        .frame(height: UIScreen.main.bounds.height*0.85)
+                        .clipShape(.rect(
+                            topLeadingRadius: 40,
+                            bottomLeadingRadius: 0,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 40
+                        ))
+//                        .ignoresSafeArea()
+                }
+                else{
+                    CameraViewReference(frameHandler: frameHandler)
+                        .frame(height: UIScreen.main.bounds.height)
+                        .ignoresSafeArea()
+                }
+                
+//                Text(showVolumeText ? "\(currentVolume) dl" : "")
+//                    .padding(30)
+//                    .foregroundColor(Color.white)
+//                    .background(Color(red: 1.0, green: 0.71, blue: 0.87))
+//                    .cornerRadius(10)
+//                    .shadow(radius: 2)
+//                    .offset(x: showVolumeText ? 0 : 1000)
+            }  //Zstack
+            .padding(-50)
+            .offset(y: -10)
             HStack(){
                 Button(action: {
                     dismiss()
                 }) {
                     Image("Return")
                         .resizable()
-                        .frame(width: 120, height: 120)
-                        .foregroundColor(.black) // Set the color of the return button
+                        .frame(width: 80, height: 80)
+//                        .foregroundColor(.black) // Set the color of the return button
                 }
-                .offset(x: -50, y: 10) // Adjust the offset to position the button
+                .offset(y: 10)// Adjust the offset to position the button
+                Spacer()
                 Button(action: {
                     if(frameHandler.captureSession.isRunning){
                         frameHandler.captureSession.stopRunning()
@@ -70,12 +78,17 @@ struct CameraUI: View {
                         .background(Color(red: 1.0, green: 0.71, blue: 0.87))
                         .foregroundColor(.white)
                         .cornerRadius(10)
-                }
-                .offset(x: -64)
-            }
-        }
-        //        .background(.clear)
-    }
-}
+                }//Button
+                .offset(x: 3)
+                Spacer()
+                Spacer()
+                Spacer()
+            }//Hstack
+            .offset(y:30)
+        }//Vstack
+        .offset(y:20)
+    } //View
+    
+}//Struct
 
 
